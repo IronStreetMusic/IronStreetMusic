c0d5b13943dcd79813b9673602bb5bb94841e034
MundoGister <mundogister@gmail.com>
2018-08-02 16:42:44 +0200

    fix: style&events attributes in discriminator schema

--- models/artist.model.js
+++ models/artist.model.js
@@ -3,7 +3,15 @@ const User = require('./user.model');
 
 
 const artistSchema = new mongoose.Schema({
-  style: String
+  style: {
+    type: String,
+    default: 'Rock',
+    required: true
+  },
+  events: {
+    type: Array,
+    required: true
+  }
 }, { 
   timestamps: true,
   discriminatorKey: 'kind'

4b6883a64f591e0ffedb3e5d54a3af18312614aa
MundoGister <mundogister@gmail.com>
2018-08-02 16:31:50 +0200

    fix: change path to signup

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users.controller');
 
-router.get('/signup', usersController.create);
-router.post('/signup', usersController.doCreate)
+router.get('/', usersController.create);
+router.post('/', usersController.doCreate)
 
 module.exports = router;
  

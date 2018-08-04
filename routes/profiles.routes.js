f64f96b84f0143a8eb11ff2c448ce789a72e706b
Pablo Gancedo <a.gancedo.pablo@gmail.com>
2018-08-04 16:14:15 +0200

    eventdelete

--- routes/profiles.routes.js
+++ routes/profiles.routes.js
@@ -7,6 +7,29 @@ const authMiddleware = require('../middlewares/auth.middleware.js')
 
 router.get('/', authMiddleware.authenticateUser, profileController.create);
 
+router.get('/id/delete', profileController.warnUser)
+router.post('/:id/delete', profileController.delete)
+
+// router.get(
+//     '/update',
+//     authMiddleware.authenticateUser,
+//     profileController.update
+//   );
+  
+//   router.post(
+//     '/:id/update',
+//     authMiddleware.authenticateUser,
+//     profileController.doUpdate
+//   );
+  
+//   router.post(
+//     '/:id/delete',
+//     authMiddleware.authenticateUser,
+//     profileController.delete
+//   );
+  
+
+
 // router.get(
 //     '/',
 //     authMiddleware.authenticateUser,

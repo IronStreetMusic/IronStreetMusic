2e4b5d2cae56641538376993807f95d67541fa2f
MundoGister <mundogister@gmail.com>
2018-08-09 19:20:36 +0200

    fix: draw marker's events in profile user map

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,4 @@
+
 (function () {
     'use strict';
     window.addEventListener('load', function () {
@@ -18,25 +19,25 @@
             "esri/map",
             "esri/dijit/Search",
             "esri/geometry/webMercatorUtils",
+            "esri/SpatialReference",
             "esri/symbols/SimpleMarkerSymbol",
             "esri/graphic",
             "esri/Color",
             "esri/geometry/Point",
             "dojo/on",
             "dojo/domReady!"
-
         ],
-        function (Map, Search, webMercatorUtils, SimpleMarkerSymbol, Graphic, Color, Point, on) {
-            // var map2 = new Map("mapEvents", {
-            //     basemap: "gray",
-            //     center: [-120.435, 46.159], // lon, lat
-            //     zoom: 7
-            // });
+        function (Map, Search, webMercatorUtils, SpatialReference, SimpleMarkerSymbol, Graphic, Color, Point, on) {
+            const map2 = new Map("map", {
+                basemap: "gray",
+                center: [-3.435, 40], // lon, lat
+                zoom: 7
+            });
 
-            // var search = new Search({
-            //     map: map2
-            // }, "search2");
-            // search.startup();
+            const search = new Search({
+                map: map2
+            }, "search");
+            search.startup();
 
             let lats = document.getElementsByName('lat1');
             let longs = document.getElementsByName('long1');
@@ -45,36 +46,16 @@
             markerSymbol.setPath("M16,4.938c-7.732,0-14,4.701-14,10.5c0,1.981,0.741,3.833,2.016,5.414L2,25.272l5.613-1.44c2.339,1.316,5.237,2.106,8.387,2.106c7.732,0,14-4.701,14-10.5S23.732,4.938,16,4.938zM16.868,21.375h-1.969v-1.889h1.969V21.375zM16.772,18.094h-1.777l-0.176-8.083h2.113L16.772,18.094z");
             markerSymbol.setColor(new Color("#00FFFF"));
 
-            on(map2, "load", function(evt) {
-                console.log("Gráficos: " + map2.graphics)
+            map2.on("load", function(evt) {
                 for (var i = 0; i < lats.length; i++) {
-                    map2.graphics.add(new Graphic(new Point(longs[i], lats[i])), markerSymbol);
+                    let coorLong = parseFloat(longs[i].value);
+                    let coorLat = parseFloat(lats[i].value);
+                    map2.graphics.add(new Graphic(new Point(coorLong, coorLat, new SpatialReference({ wkid: 4326 })), markerSymbol));
                 }
             });
-
-            // console.log("Gráficos: " + map2.graphics)
-            // for (var i = 0; i < lats.length; i++) {
-            //     map2.graphics.add(new Graphic(new Point(longs[i], lats[i])), markerSymbol);
-            // }
-
-            //map.graphics.add(new Graphic(, symbol));
 
         });
     }, false);
 
     
 })();
-
-// function showFormVis(){
-//     if (document.getElementById("VIS").style.display==="none"){
-//         document.getElementById("VIS").style.display ="block";
-//         document.getElementById("VIP").style.display = "none";
-//     }
-// }
-
-// function showFormVip() {
-//     if (document.getElementById("VIP").style.display === "none") {
-//         document.getElementById("VIP").style.display = "block";
-//         document.getElementById("VIS").style.display = "none";
-//     }
-// }

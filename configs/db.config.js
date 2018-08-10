0b00e8dfe58b201b4f282b69e9ebdfb36c526037
MundoGister <mundogister@gmail.com>
2018-08-10 08:59:11 +0200

    change config

--- configs/db.config.js
+++ configs/db.config.js
@@ -3,17 +3,17 @@ require('dotenv').config();
 
 // Credenciales de desarrollo
 const mongoose = require('mongoose');
-// const DB_NAME = 'iron-street-music';
-// const MONGO_URI = `mongodb://localhost:27017/${DB_NAME}`;
+const DB_NAME = 'iron-street-music';
+const MONGO_URI = `mongodb://localhost:27017/${DB_NAME}`;
 //
 
 
 //   Credenciales de producción
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_HOSTNAME = process.env.DB_HOSTNAME;
-const DB_PORT = process.env.DB_PORT;
-const MONGO_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOSTNAME}:${DB_PORT}/${DB_NAME}`;
+// const DB_USER = process.env.DB_USER;
+// const DB_PASSWORD = process.env.DB_PASSWORD;
+// const DB_HOSTNAME = process.env.DB_HOSTNAME;
+// const DB_PORT = process.env.DB_PORT;
+// const MONGO_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOSTNAME}:${DB_PORT}/${DB_NAME}`;
 
 
 mongoose.connect(MONGO_URI);

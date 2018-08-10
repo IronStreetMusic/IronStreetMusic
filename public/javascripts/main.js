2d8978aec6b9ca8d6ba3bf897f6aaca2435daf8f
MundoGister <mundogister@gmail.com>
2018-08-10 20:54:18 +0200

    feat: change basemap

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -16,22 +16,27 @@
         });
 
         require([
-            "esri/map",
-            "esri/dijit/Search",
-            "esri/geometry/webMercatorUtils",
-            "esri/SpatialReference",
-            "esri/symbols/SimpleMarkerSymbol",
-            "esri/graphic",
-            "esri/Color",
-            "esri/geometry/Point",
-            "dojo/on",
-            "dojo/domReady!"
+                "esri/map",
+                "esri/dijit/Search",
+                "esri/geometry/webMercatorUtils",
+                "esri/SpatialReference",
+                "esri/symbols/SimpleMarkerSymbol",
+                "esri/symbols/PictureMarkerSymbol",
+                "esri/InfoTemplate",
+                "esri/graphic",
+                "esri/Color",
+                "esri/geometry/Point",
+                "dojo/on",
+                "dojo/domReady!"
         ],
-        function (Map, Search, webMercatorUtils, SpatialReference, SimpleMarkerSymbol, Graphic, Color, Point, on) {
+        function (Map, Search, webMercatorUtils,
+            SpatialReference, SimpleMarkerSymbol, PictureMarkerSymbol,
+            InfoTemplate, Graphic, Color, Point, on) {
+                
             const map2 = new Map("mapUser", {
-                basemap: "gray",
-                center: [-3.435, 40], // lon, lat
-                zoom: 7
+            basemap: "streets-night-vector",
+            center: [-3.435, 40], // lon, lat
+            zoom: 7
             });
 
             const search = new Search({
@@ -42,15 +47,34 @@
             let lats = document.getElementsByName('lat1');
             let longs = document.getElementsByName('long1');
 
-            let markerSymbol = new SimpleMarkerSymbol();
-            markerSymbol.setPath("M16,4.938c-7.732,0-14,4.701-14,10.5c0,1.981,0.741,3.833,2.016,5.414L2,25.272l5.613-1.44c2.339,1.316,5.237,2.106,8.387,2.106c7.732,0,14-4.701,14-10.5S23.732,4.938,16,4.938zM16.868,21.375h-1.969v-1.889h1.969V21.375zM16.772,18.094h-1.777l-0.176-8.083h2.113L16.772,18.094z");
-            markerSymbol.setColor(new Color("#00FFFF"));
+            let eventsName = document.getElementsByName('eventName');
+            let dates = document.getElementsByName('date');
+            let addresses = document.getElementsByName('address');
+
+            let pictureMarkerSymbol = new PictureMarkerSymbol();
+            // pictureMarkerSymbol.url = "https://image.flaticon.com/icons/png/512/95/95223.png";
+            // pictureMarkerSymbol.url = "https://image.flaticon.com/icons/png/512/70/70534.png";
+            pictureMarkerSymbol.url = "/images/icono-conciertos.svg";
+            pictureMarkerSymbol.width = 30;
+            pictureMarkerSymbol.height = 30;
 
             map2.on("load", function(evt) {
                 for (var i = 0; i < lats.length; i++) {
                     let coorLong = parseFloat(longs[i].value);
                     let coorLat = parseFloat(lats[i].value);
-                    map2.graphics.add(new Graphic(new Point(coorLong, coorLat, new SpatialReference({ wkid: 4326 })), markerSymbol));
+                    let point = new Point(coorLong, coorLat, new SpatialReference({
+                        wkid: 4326
+                    }));
+
+                    let content = {
+                        title: eventsName[i].innerText,
+                        content: "<b>Dirección:</b> " + addresses[i].innerText + "<br><b>Fecha:</b> " + dates[i].innerText
+                    };
+                    let popup = new InfoTemplate(content);
+
+                    let graphic = new Graphic(point, pictureMarkerSymbol);
+                    graphic.setInfoTemplate(popup);
+                    map2.graphics.add(graphic);
                 }
             });
 
--- public/javascripts/showMapArtist.js
+++ public/javascripts/showMapArtist.js
@@ -17,10 +17,12 @@
                 "dojo/on",
                 "dojo/domReady!"
             ],
-            function (Map, Search, webMercatorUtils, SpatialReference, SimpleMarkerSymbol, PictureMarkerSymbol, InfoTemplate, Graphic, Color, Point, on) {
+            function (Map, Search, webMercatorUtils,
+                 SpatialReference, SimpleMarkerSymbol, PictureMarkerSymbol, 
+                 InfoTemplate, Graphic, Color, Point, on) {
                 
                 const mapArtist = new Map("mapArtist", {
-                    basemap: "gray",
+                    basemap: "streets-night-vector",
                     center: [-3.435, 40], // lon, lat
                     zoom: 7
                 });
@@ -33,15 +35,16 @@
                 let lats = document.getElementsByName('lat1');
                 let longs = document.getElementsByName('long1');
 
+                let eventsName = document.getElementsByName('eventName');
+                let dates = document.getElementsByName('date');
+                let addresses = document.getElementsByName('address');
+
                 let pictureMarkerSymbol = new PictureMarkerSymbol();
                 // pictureMarkerSymbol.url = "https://image.flaticon.com/icons/png/512/95/95223.png";
-                pictureMarkerSymbol.url = "https://image.flaticon.com/icons/png/512/70/70534.png";
+                // pictureMarkerSymbol.url = "https://image.flaticon.com/icons/png/512/70/70534.png";
+                pictureMarkerSymbol.url = "/images/icono-conciertos.svg";
                 pictureMarkerSymbol.width = 30;
                 pictureMarkerSymbol.height = 30;
-
-                let markerSymbol = new SimpleMarkerSymbol();
-                markerSymbol.setPath("M16,4.938c-7.732,0-14,4.701-14,10.5c0,1.981,0.741,3.833,2.016,5.414L2,25.272l5.613-1.44c2.339,1.316,5.237,2.106,8.387,2.106c7.732,0,14-4.701,14-10.5S23.732,4.938,16,4.938zM16.868,21.375h-1.969v-1.889h1.969V21.375zM16.772,18.094h-1.777l-0.176-8.083h2.113L16.772,18.094z");
-                markerSymbol.setColor(new Color("#00FFFF"));
 
                 mapArtist.on("load", function (evt) {
                     for (var i = 0; i < lats.length; i++) {
@@ -50,16 +53,14 @@
                         let point = new Point(coorLong, coorLat, new SpatialReference({wkid: 4326}));
 
                         let content = {
-                            title: "Evento",
-                            content: "Buenas tardes"
+                            title: eventsName[i].innerText,
+                            content: "<b>Dirección:</b> " + addresses[i].innerText + "<br><b>Fecha:</b> " + dates[i].innerText
                         };
                         let popup = new InfoTemplate(content);
 
                         let graphic = new Graphic(point, pictureMarkerSymbol);
                         graphic.setInfoTemplate(popup);
                         mapArtist.graphics.add(graphic);
-
-
                     }
                 });
 

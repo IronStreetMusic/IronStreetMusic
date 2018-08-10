e92c9c88b34bb36165220bdb8839a7cddb937619
MundoGister <mundogister@gmail.com>
2018-08-10 10:41:01 +0200

    feat: add icon to events

--- public/javascripts/showMapArtist.js
+++ public/javascripts/showMapArtist.js
@@ -9,13 +9,14 @@
                 "esri/geometry/webMercatorUtils",
                 "esri/SpatialReference",
                 "esri/symbols/SimpleMarkerSymbol",
+                "esri/symbols/PictureMarkerSymbol",
                 "esri/graphic",
                 "esri/Color",
                 "esri/geometry/Point",
                 "dojo/on",
                 "dojo/domReady!"
             ],
-            function (Map, Search, webMercatorUtils, SpatialReference, SimpleMarkerSymbol, Graphic, Color, Point, on) {
+            function (Map, Search, webMercatorUtils, SpatialReference, SimpleMarkerSymbol, PictureMarkerSymbol, Graphic, Color, Point, on) {
                 
                 const mapArtist = new Map("mapArtist", {
                     basemap: "gray",
@@ -31,6 +32,12 @@
                 let lats = document.getElementsByName('lat1');
                 let longs = document.getElementsByName('long1');
 
+                let pictureMarkerSymbol = new PictureMarkerSymbol();
+                // pictureMarkerSymbol.url = "https://image.flaticon.com/icons/png/512/95/95223.png";
+                pictureMarkerSymbol.url = "https://image.flaticon.com/icons/png/512/70/70534.png";
+                pictureMarkerSymbol.width = 30;
+                pictureMarkerSymbol.height = 30;
+
                 let markerSymbol = new SimpleMarkerSymbol();
                 markerSymbol.setPath("M16,4.938c-7.732,0-14,4.701-14,10.5c0,1.981,0.741,3.833,2.016,5.414L2,25.272l5.613-1.44c2.339,1.316,5.237,2.106,8.387,2.106c7.732,0,14-4.701,14-10.5S23.732,4.938,16,4.938zM16.868,21.375h-1.969v-1.889h1.969V21.375zM16.772,18.094h-1.777l-0.176-8.083h2.113L16.772,18.094z");
                 markerSymbol.setColor(new Color("#00FFFF"));
@@ -41,7 +48,7 @@
                         let coorLat = parseFloat(lats[i].value);
                         mapArtist.graphics.add(new Graphic(new Point(coorLong, coorLat, new SpatialReference({
                             wkid: 4326
-                        })), markerSymbol));
+                        })), pictureMarkerSymbol));
                     }
                 });
 
